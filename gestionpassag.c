abe99bb1c20fa6ed9242464439293e68a75450eb
BUNANA KAUNDA SAMUEL <22bk030esisalama.org>
2024-06-06 22:26:56 +0200

    ajout de procedure

--- gestionpassag.c
+++ gestionpassag.c
@@ -1,16 +1,96 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct passager
+{
+    char NOM[50];
+    char PRE_NOM[50];
+    char addresse[100];
+    char telephone[20];
+    char email[50];
+    int nombre_reservation ;
+}passager;
+
+void ajouter_passager(passager *passagers , int **nombre_passagers)
+{
+        printf("veuiller entrer votre NOM : ");
+        scanf("%s" , passagers[ ** nombre_passagers].NOM);
+
+        printf("veuiller entrer votre PRE_NOM : ");
+        scanf("%s" , passagers[ ** nombre_passagers].PRE_NOM);
+
+        printf("veuiller entrer votre addresse : ");
+        scanf("%s" , passagers[ ** nombre_passagers].addresse);
+
+        printf("veuiller entrer votre numero de telephone : ");
+        scanf("%s" , passagers[ ** nombre_passagers].telephone);
+
+        printf("veuiller entrer votre email : ");
+        scanf("%s" , passagers[ ** nombre_passagers].email);
+
+        passagers [ **nombre_passagers].nombre_reservation = 0;
+        (*nombre_passagers) ++
+
+};
+
+void afficher_tous_les_passagers(passager *passagers , int nb_passagers)
+{
+    for (int i = 0; i < nb_passagers; i++)
+    {
+        afficher_passager(passagers[i]);
+    }
+    
+}
+
+// la fonction principale prend en parametre tout les procedures 
+
 
 int main ()
 {
-        char nom;
-        char postnom;
-    printf("entrer votre nom : ");
-    scanf("%s", nom);
-    printf("entrer votre post-nom : ");
-    scanf("%s", postnom);
+     passager passagers[100];
+     int nb_passagers = 0;
 
+     int choix;
+     do
+     {
+        printf("\t************MENU\t***********\n");
+        printf("1. Ajouter un passagers \n");
+        printf("2. Afficher un passager\n");
+        printf("3. Afficher tous les passagers \n");
+        printf("4. Quitter  \n");
+
+        printf("enter votre choix : ");
+        scanf("%d", &choix);
+
+        switch (choix)
+        {
+        case 1:
+            ajouter_passager(passagers , &nb_passagers);
+            break;
+        case 2:
+        int id_passager ;
+            printf("entrer l'Id du passager : ");
+            scanf("%d", &id_passager);
+
+            if (id_passager >= 0 && id_passager < nb_passagers)
+            {
+                afficher_passager(passagers[id_passager]);
+            }else{
+                printf("ID du passager invalide .\n ");
+            }  
+            break;
+
+        case 3:
+            afficher_tous_les_passagers(passagers, nb_passagers);
+            break;
+        case 4:
+            break; 
+        default:
+        printf("choix invalide .\n");
+            break;
+        }
+     } while (choix !=4);
+       
     return 0;
 
 }

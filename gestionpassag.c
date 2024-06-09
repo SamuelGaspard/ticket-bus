4aa41c8a660248bf07bb22a7b63e79a0a702946e
BUNANA KAUNDA SAMUEL <22bk030esisalama.org>
2024-06-09 18:40:31 +0200

    quelque modfification sur l'enregistrrement du passager

--- gestionpassag.c
+++ gestionpassag.c
@@ -27,12 +27,93 @@ void ajouter_passager(passager *passagers , int *nbpassagers)
     passager * nouveaupassager = malloc(sizeof(passager));
 
     //saisie des information du passagers 
-    printf("")
+    printf("Entrer le nom du passager  : ");
+    scanf("%s", nouveaupassager ->NOM);
+    printf("Entrer le pre-nom du passager  : ");
+    scanf("%s", nouveaupassager ->PRE_NOM);
+    printf("Entrer le numero du passager  : ");
+    scanf("%s", nouveaupassager ->telephone);
+    printf("Entrer email du passager  : ");
+    scanf("%s", nouveaupassager ->email);
+    printf("Entrer le nombre de(s) passager  : ");
+    scanf("%d", nouveaupassager ->nombre_reservation);
+
+    nouveaupassager ->nbpassagers = 0;
+    passagers[*nbpassagers] = *nouveaupassager;
+    *nbpassagers += 1;
+}
+
+//la fonction pour afficher les informatio du passagers 
+
+void afficherpassager(passager *passager)
+{
+    printf("NOM : %s %s \n", passager ->NOM , passager ->PRE_NOM);
+    printf("Email : %s \n " , passager ->email);
+    printf("telephone : %s \n " , passager ->telephone);
+    printf("nombre reserve(s) : %d\n " , passager ->nombre_reservation);
+
+    //afficher l'historique du reservation
+    if (passager ->nombre_reservation > 0)
+    {
+        printf("Historique de reservation : \n");
+        for (int i = 0; i < passager ->nombre_reservation; i++)
+        {
+            struct Reservation* reservation = passager ->reservations[i];
+            printf(" - Date de voyage : %s \n ", reservation ->datevoyage);
+            printf(" - numero bus : %s \n ", reservation ->numerobus);
+            printf(" - place : %s \n ", reservation->place);
+            
+        }
+        else{
+            printf("ce passager n'a aucune reservation . \n");
+        }
+        
+    }
+     
+}
+
+//ajouter une reservation a un passager 
+
+void ajouterreservation(passager*passagers , int nbpassagers)
+{
+    int choixpassager;
+    printf("entrer le numero du passager : ");
+    scanf("%d", &choixpassager);
+    if (choixpassager < 1 || choixpassager > nbpassagers)
+    {
+        printf("numero de passager invalide \n ");
+        return;
+    }
+    passager* passager = &passagers[choixpassager -1];
+    //allocation d'une memoire pour ajouter une reservation 
+    struct Reservation* nouvelleReservation = malloc(sizeof(Reservation));
+
+    //la saisie de informations de reservation 
+    printf("entrer la date du voyage : ");
+    scanf("%s", nouvelleReservation ->datevoyage);
+    printf("entrer le numero du bus : ");
+    scanf("%s", nouvelleReservation ->numerobus);
+    printf("entrer la place : ");
+    scanf("%s", nouvelleReservation ->place);
+
+    //ajouter la nouvelle reservation dans l'historique du passager 
+    passager ->reservations[passager ->nombre_reservation] = nouvelleReservation;
+    passager ->nombre_reservation += 1;
+
+    printf("reservation ajouter avec succes ! \n");
+
+    
 }
 
 int main()
 {
-    printf("");
+    //table pour stocker les passagers
+    void ajouter_passager();
+    void afficherpassager();
+    void ajouterreservation();
+    
+
+    passager passagers[100];
     return 0;
 
 }
